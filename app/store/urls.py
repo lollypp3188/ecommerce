88d9de74eff0986e9f378412af038fb1f2250ad2
lollypp3188 <annayakymovaaa@gmail.com>
2024-09-21 14:38:09 +0300

    feat: add urls for store

--- app/store/urls.py
+++ app/store/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
   path('', views.store, name='store'),
+  path('product/<slug:product_slug>/', views.product_info, name='product-info'),
+  path('category/<slug:category_slug>/', views.list_category, name='list-category'),
 ]
