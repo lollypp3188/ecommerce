57e73656adce69ee9dc0fbdf9b86d3c83b881b5e
lollypp3188 <annayakymovaaa@gmail.com>
2024-09-26 15:57:56 +0300

    feat: add account app

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'mathfilters',
     'store',
     'cart',
+    'account',
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('cart/', include('cart.urls')),
+    path('account/', include('account.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
